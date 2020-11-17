c54372e3049352d4e0d4a45436319e28eda0003a
Jakob Ruckel <jakob@ruckel.de>
2020-11-17 14:34:50 +0100

    Format code

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include <omp.h>
+#include <stdio.h>
 
 int main() {
 #pragma omp parallel
-  printf("Hallo, ich bin Thread %d von %d.\n",
-      omp_get_thread_num(),
-      omp_get_num_threads());
+  printf("Hallo, ich bin Thread %d von %d.\n", omp_get_thread_num(),
+         omp_get_num_threads());
   return 0;
 }
