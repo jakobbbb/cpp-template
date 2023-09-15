d5cc0bdf3b2673d6cf1611d29258554e12de688e
Jakob Ruckel <jakob@ruckel.de>
2023-09-15 13:08:57 +0200

    Format code

--- source/tests.cpp
+++ source/tests.cpp
@@ -2,5 +2,7 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("hello", "[hello]") {
-    SECTION("42 is 42", "[42]") { REQUIRE(42 == 42); }
+    SECTION("42 is 42", "[42]") {
+        REQUIRE(42 == 42);
+    }
 }
